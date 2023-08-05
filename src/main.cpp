55ab613b0e4bf080fea9d183417ea09c12ec43c0
Arc0926 <Arcanine0926@gmail.com>
2023-08-05 01:01:21 -0400

    main in progress

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,17 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "compression-algorithms/HuffmanCoding.h"
+using namespace std;
+
 int main() {
+    cout << "Enter .txt file path:" << endl;
+    string filePath;
+    cin >> filePath;
+    ifstream input(filePath);
+    ofstream output("../test-io/output-files/main-output.txt");
+
+
 	return 0;
 }
 
